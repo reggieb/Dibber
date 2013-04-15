2afaa224eaeb1ed48be29c53464e0694ab0998be
Rob Nichols <git@nicholshayes.co.uk>
2013-04-15 15:51:32 +0100

    Fixes bug in overwrite code

--- lib/dibber/version.rb
+++ lib/dibber/version.rb
@@ -1,11 +1,15 @@
 module Dibber
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
 end
 
 # History
 # =======
+# 
+# 0.2.1 Fixes bug in overwrite code
+#       System was not correctly identifying when an object was a new record
+# 
 # 0.2.0 Stops overwriting existing entries unless explicitly directed to
-#       No need to specify :overwrite => true when creating a new Seeder if
+#       Now need to specify :overwrite => true when creating a new Seeder if
 #       existing entries are to be overwritten.
 # 
 # 0.1.1 Working version
