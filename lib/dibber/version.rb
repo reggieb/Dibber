f583fb1e1c84446e6d70cdc47751d37d419a30f3
Rob Nichols <git@nicholshayes.co.uk>
2013-08-01 11:52:58 +0100

    Bump version to 0.2.2

--- lib/dibber/version.rb
+++ lib/dibber/version.rb
@@ -1,10 +1,13 @@
 module Dibber
-  VERSION = "0.2.1"
+  VERSION = "0.2.2"
 end
 
 # History
 # =======
-# 
+# 0.2.2 Minor bug fix. There was an error in the example. Also a small bit
+#       of house keeping (adding license to gemspec, and removing Gemfile.lock
+#       from repository.
+#
 # 0.2.1 Fixes bug in overwrite code
 #       System was not correctly identifying when an object was a new record
 # 
